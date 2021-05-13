2057702449410a00d3a5658348911042cab0c1bd
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-13 07:15:48 +0100

    First Commit

---
+++ src/components/LoginButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="btn btn-primary btn-block"
+    v-if="!$auth.isAuthenticated"
+    @click="login"
+  >
+    Log In
+  </button>
+</template>
+
+<script>
+export default {
+  name: "LoginButton",
+  methods: {
+    login() {
+      this.$auth.loginWithRedirect(
+      { screen_hint: "signup", }
+      );
+    },
+  },
+};
+</script>
