fe1a50531ff09c25415f76c4b14f97dd70e78cd3
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-13 07:39:45 +0100

    Buttons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-light p-3 text-center">
     <div class="logo"></div>
     <p>
-      Sample project provided by
+      Demo project to install
       <a target="_blank" rel="noopener noreferrer" href="https://auth0.com">
         Auth0
       </a>
